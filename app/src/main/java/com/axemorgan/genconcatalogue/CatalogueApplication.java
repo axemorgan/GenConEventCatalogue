8a54bcfd67b1354db89db7347de828f76a317bd0
Alex Morgan <axemorgan@gmail.com>
2017-08-04 22:18:08 -0400

    Successfully reading from the workbook without running out of memory!

--- app/src/main/java/com/axemorgan/genconcatalogue/CatalogueApplication.java
+++ app/src/main/java/com/axemorgan/genconcatalogue/CatalogueApplication.java
@@ -14,6 +14,9 @@ public class CatalogueApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLInputFactory", "com.fasterxml.aalto.stax.InputFactoryImpl");
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLOutputFactory", "com.fasterxml.aalto.stax.OutputFactoryImpl");
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLEventFactory", "com.fasterxml.aalto.stax.EventFactoryImpl");
     }
 
     public Retrofit getRetrofit() {
